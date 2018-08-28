1054eb25141ed5e30479796ae88a17f752fe7ec9
nicolas33450 <40290258+nicolas33450@users.noreply.github.com>
2018-08-28 15:44:19 +0200

    g

---
+++ foncion_chr.php
@@ -0,0 +1,7 @@
+<?php
+// 65 et 90 lettre majuscule
+echo chr(66);
+
+$a = 97;
+
+echo chr($a);
--- image/captcha.php
+++ image/captcha.php
@@ -6,11 +6,22 @@ header("Content-type: image/jpg");
 
 $img_tab = ['fond_1.jpg', 'fond_2.jpg', 'fond_3.jpg','fond_4.jpg','fond_5.jpg', 'fond_6.jpg'];
 
-$nb = mt_rand(0,5);
+$img = mt_rand(0,5);
 
 // chargement de l'image
-$image = imagecreatefromjpeg($img_tab[$nb]);
+$image = imagecreatefromjpeg($img_tab[$img]);
 
 imagejpeg($image);
-	
+
+$nb1 = mt_rand(65,90);
+
+chr($nb1);
+
+$text_color = imagecolorallocate ($image, 255, 255, 255);
+
+//imagettftext (image destination, taille, inclinaison, pos_x, pos_y, couleur, chemin police ttf, texte a écrire);
+imagettftext ($image, 10, 20, 50, 20, $text_color, "./desyrel.ttf", $nb1);
+
+imagejpeg($image);
+
 ?>
